0aedac78e5973dcb0e0e89523dc2d98017d83bc6
Mateusz <44375816+matmieciek@users.noreply.github.com>
2019-01-07 15:42:34 +0100

    Add files via upload

---
+++ firstsem/script.js
@@ -0,0 +1,90 @@
+//main page project preview
+//first
+let but1 = document.querySelector(".item1");
+let but1p = document.querySelector(".item1p");
+
+but1.addEventListener("mouseenter", picshow);
+but1.addEventListener("mouseleave", pichide);
+
+function picshow() {
+
+but1p.classList.add("item1psee");
+ }
+function pichide() {
+but1p.classList.remove("item1psee");
+
+ }
+
+//second
+
+let but2 = document.querySelector(".item2");
+let but2p = document.querySelector(".item2p");
+
+
+but2.addEventListener("mouseenter", picshow2);
+but2.addEventListener("mouseleave", pichide2);
+
+function picshow2() {
+
+but2p.classList.add("item2psee");
+ }
+function pichide2() {
+but2p.classList.remove("item2psee");
+
+ }
+
+//third
+
+let but3 = document.querySelector(".item3");
+let but3p = document.querySelector(".item3p");
+
+but3.addEventListener("mouseenter", picshow3);
+but3.addEventListener("mouseleave", pichide3);
+
+function picshow3() {
+
+but3p.classList.add("item3psee");
+ }
+function pichide3() {
+but3p.classList.remove("item3psee");
+
+ }
+//fourth
+
+let but4 = document.querySelector(".item4");
+let but4p = document.querySelector(".item4p");
+
+but4.addEventListener("mouseenter", picshow4);
+but4.addEventListener("mouseleave", pichide4);
+
+function picshow4() {
+
+but4p.classList.add("item4psee");
+ }
+function pichide4() {
+but4p.classList.remove("item4psee");
+
+ }
+//fift5
+
+let but5 = document.querySelector(".item5");
+let but5p = document.querySelector(".item5p");
+
+but5.addEventListener("mouseenter", picshow5);
+but5.addEventListener("mouseleave", pichide5);
+
+function picshow5() {
+
+but5p.classList.add("item5psee");
+ }
+function pichide5() {
+but5p.classList.remove("item5psee");
+
+ }
+
+///i'm sure there is an easy way to simplify this code maybe using i and switch
+
+
+
+
+
